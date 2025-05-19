652a6ace62e7c64b8bfc004809fcc759333bfe8d
wi113506 <86154@student.pb.edu.pl>
2025-05-19 22:19:48 +0200

    Widok Register i Login

--- Chatter/Controllers/UserController.cs
+++ Chatter/Controllers/UserController.cs
@@ -9,12 +9,15 @@ namespace Chatter.Controllers
     public class UserController : Controller
     {
         private readonly IUserService _userService;
+        private readonly SignInManager<User> _signInManager;
 
         public UserController(IUserService userService)
         {
             _userService = userService;
+            _signInManager = signInManager;
         }
 
+        [HttpPost]
         public async Task<IActionResult> Register(string username, string password, string email)
         {
             var user = new User { UserName = username, Email = email };
@@ -30,6 +33,7 @@ namespace Chatter.Controllers
             }
         }
 
+        [HttpPost]
         public async Task<IActionResult> Login(string username, string password)
         {
             var result = await _userService.SignInUserAsync(username, password);
@@ -42,6 +46,12 @@ namespace Chatter.Controllers
             {
                 return View();
             }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
